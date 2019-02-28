e7585d503529df6b2d4c113b0c34b63f0edbb666
Unknown <roxxanrebirth@gmail.com>
2019-02-27 19:12:51 -0500

    Initial Mod

---
+++ src/main/java/nerdhub/deathmaps/mixins/MixinPlayerManager.java
@@ -0,0 +1,30 @@
+package nerdhub.deathmaps.mixins;
+
+import net.minecraft.item.FilledMapItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.map.MapIcon;
+import net.minecraft.item.map.MapState;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.StringTextComponent;
+import net.minecraft.text.Style;
+import net.minecraft.text.TextFormat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayerEntity.class)
+public class MixinPlayerManager {
+
+    @Inject(method = "method_14203", at = @At("RETURN"))
+    private void onRespawn(ServerPlayerEntity serverPlayerEntity_1, boolean boolean_1, CallbackInfo ci){
+
+        if (!boolean_1){
+            ItemStack itemStack = FilledMapItem.method_8005(serverPlayerEntity_1.getServerWorld(), serverPlayerEntity_1.getPos().getX(), serverPlayerEntity_1.getPos().getZ(), (byte) 2,true,true);
+            FilledMapItem.method_8002(serverPlayerEntity_1.getServerWorld(), itemStack);
+            MapState.method_110(itemStack, serverPlayerEntity_1.getPos(),"Death", MapIcon.Type.TARGET_X);
+            itemStack.setDisplayName(new StringTextComponent("Grave Digger's Map").setStyle(new Style().setColor(TextFormat.RED)));
+            ((ServerPlayerEntity) (Object) this).inventory.insertStack(itemStack);
+        }
+    }
+}
